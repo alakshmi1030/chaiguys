198dc815b87e4615c6a7f67f87d52b7d04d4c42c
Anand Lakshminarayan <anand.lakshminarayan@gmail.com>
2015-03-16 17:42:32 -0700

    catering request form basic implementation working

--- start.py
+++ start.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
+import smtplib
+import mimetypes
+import os
 
-app = Flask("start")
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'arcsmtpserver@gmail.com'
+MAIL_PASSWORD = 'cha1nsaw'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+mail = Mail(app)
 
 @app.route("/")
 def index():
@@ -11,6 +25,10 @@ def index():
 @app.route("/services")
 def services():
 	return render_template("services.html")
+
+@app.route("/servicesdone")
+def servicedone():
+	return render_template("servicesdone.html")
 
 @app.route("/submitted", methods=['POST'])
 def submitted():
@@ -21,4 +39,15 @@ def submitted():
 	phone = "Phone Number: " + request.form["phoneNumber"]
 	extra = "Extra text: " + request.form["extraText"]
 
-app.run()
+	everything = name + "\n" + email + "\n" + organization + "\n" + phone + "\n" + extra
+
+	msg = Message('Catering request received - ' + email,sender="test@gmail.com",recipients=["calchaiguys@gmail.com"])
+	msg.body = everything
+
+	mail.send(msg)
+	return render_template("servicesdone.html")
+
+if __name__ == "__main__":
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
